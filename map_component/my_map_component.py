f26c527e3352d0aad826a53d34dcadc5d6578a23
hftyvold <hftyvold@gmail.com>
2024-10-23 12:30:02 +0000

    remove build from gitignore

--- map_component/my_map_component.py
+++ map_component/my_map_component.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 # Declare the Streamlit component only once
 _component_func = components.declare_component(
-    "my_map_component",  # The name
+    "test",  # The name
     path=os.path.join(os.path.dirname(__file__), "frontend/build")  # Path to the frontend build
 )
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-#from map_component import show_map
+from map_component import show_map
 import os
 
 st.title("Show Your Position on a Map")
 st.write(f'{os.getcwd()}')
-#show_map()
+show_map()
