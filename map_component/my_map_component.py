4e5a2c586c088239b7a317263e3329a42c94a6b3
hftyvold <hftyvold@gmail.com>
2024-10-23 12:01:28 +0000

    test

--- map_component/my_map_component.py
+++ map_component/my_map_component.py
@@ -1,12 +1,12 @@
+import os
+import streamlit as st
 import streamlit.components.v1 as components
 
-# Create a Streamlit component to render the map
-if "_component_func" not in st.session_state:
-    _component_func = components.declare_component(
-        "my_map_component",
-        path="my_map_component/frontend/build"
-    )
-    st.session_state._component_func = _component_func
+# Declare the Streamlit component only once
+_component_func = components.declare_component(
+    "my_map_component",  # The name
+    path=os.path.join(os.path.dirname(__file__), "frontend/build")  # Path to the frontend build
+)
 
 def show_map():
     return _component_func()
